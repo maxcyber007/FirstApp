dee66187c9f99821907a2da0238553fc804e5d5e
Anuchat Rungsiyanon <rungsiyanon@gmail.com>
2021-12-20 13:55:04 +0700

    Version 2.0

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
     return ( 
         <View style = { styles.container } >
-        <Text style = { styles.textRedbold }>Mr.Anuchat  Rungsiyanon</Text>
+        <Text style = { styles.textRedbold }>React Native</Text>
         <Text>Mr.Anuchat  Rungsiyanon</Text>
         <StatusBar style="auto" />
         </View>
